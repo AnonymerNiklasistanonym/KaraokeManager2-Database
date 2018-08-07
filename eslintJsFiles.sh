54278d0174201ecf8b20931745c8235ae2c1b870
AnonymerNiklasistanonym <niklas.mikeler@gmail.com>
2018-08-07 02:49:10 +0200

    updated js formatting to standard from eslint

--- eslintJsFiles.sh
+++
@@ -1,5 +0,0 @@
-# install eslint (npm install eslint -g)
-eslint '.\index.js' --fix
-eslint '.\classes\database_helper.js' --fix
-eslint '.\classes\database_table_parser.js' --fix
-eslint '.\classes\database_table_parsing_class.js' --fix
---
+++ formatAndFixJsFiles.sh
@@ -0,0 +1,5 @@
+# install eslint (npm install standard --global,  npm install standard --save-dev)
+standard  '.\index.js' --fix
+standard  '.\classes\database_helper.js' --fix
+standard  '.\classes\database_table_parser.js' --fix
+standard  '.\classes\database_table_parsing_class.js' --fix
